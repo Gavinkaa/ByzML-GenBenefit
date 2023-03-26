3a91a67c50ff232fc791f75e9d46f0d02a5c0947
Gavinkaa <ludovic.burnier@epfl.ch>
2023-03-26 21:01:40 +0200

    cwtm corrected for f=0

--- src/byzml_genbenefit/aggregators/cwtm.py
+++ src/byzml_genbenefit/aggregators/cwtm.py
@@ -28,7 +28,7 @@ class CWTMAggregator(aggregator.Aggregator):
 
             # Remove the f lowest and f highest values in dimension 0
             stacked_tensor, _ = torch.sort(stacked_tensor, dim=0)
-            if f != 0:
+            if f != 0 and f * 2 < stacked_tensor.shape[0]:
                 stacked_tensor = stacked_tensor[f:-f]
 
             avg_tensor = torch.mean(stacked_tensor, dim=0)
