f4d4fd7b0e253e80c846a0ad8dead15d04043555
Gavinkaa <ludovic.burnier@epfl.ch>
2023-04-04 11:08:13 +0200

    adding some normalization

--- src/byzml_genbenefit/data/cifar10.py
+++ src/byzml_genbenefit/data/cifar10.py
@@ -7,16 +7,32 @@ DATA_FOLDER_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../
 
 
 def get_dataset():
-    """Returns the CIFAR10 dataset as a tuple of (train_dataset, test_dataset)
+    """Returns the CIFAR10 dataset as a tuple of (train_dataset, test_dataset). This dataset is normalized.
 
     Returns:
         train_dataset (torchvision.datasets.CIFAR10): The training dataset
         test_dataset (torchvision.datasets.CIFAR10): The test dataset
     """
-    train_dataset = torchvision.datasets.CIFAR10(root=DATA_FOLDER_PATH, train=True, transform=transforms.ToTensor(),
+
+    # values where first taken from https://github.com/kuangliu/pytorch-cifar/issues/19 and then
+    # verified using the code below (which is commented out).
+    normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2435, 0.2616])
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    train_dataset = torchvision.datasets.CIFAR10(root=DATA_FOLDER_PATH, train=True, transform=transform,
                                                  download=True)
-    test_dataset = torchvision.datasets.CIFAR10(root=DATA_FOLDER_PATH, train=False, transform=transforms.ToTensor(),
+    test_dataset = torchvision.datasets.CIFAR10(root=DATA_FOLDER_PATH, train=False, transform=transform,
                                                 download=True)
+
+    # print(train_dataset.data.shape)
+    # print(train_dataset.data.mean(axis=(0, 1, 2)) / 255)
+    # print(train_dataset.data.std(axis=(0, 1, 2)) / 255)
+    # >>> [0.49139968 0.48215841 0.44653091]
+    # >>> [0.24703223 0.24348513 0.26158784]
+
     return train_dataset, test_dataset
 
 
