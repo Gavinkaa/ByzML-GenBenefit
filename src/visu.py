a5b9048320b26154868e7b7c9c0596c3cc57d507
Gavinkaa <ludovic.burnier@epfl.ch>
2023-04-25 12:32:22 +0200

    updated visu with gap and loss

--- src/visu.py
+++ src/visu.py
@@ -10,7 +10,7 @@ OUTPUT_FOLDER = '../results/CIFAR-10'
 
 
 def plot():
-    plt.rcParams['figure.figsize'] = (10, 8)
+    plt.rcParams['figure.figsize'] = (10, 8 * 3)
 
     files = os.listdir(Path(INPUT_FOLDER))
 
@@ -53,28 +53,9 @@ def plot():
         if aggregator == 'None':
             continue
 
-        plt.figure()
+        fig, ax = plt.subplots(3, 1)
 
-        for nb_byz in group['nb_byz'].unique():
-            group_byz = group[group['nb_byz'] == nb_byz]
-
-            acc_test_mean = group_byz.groupby('epoch')['accuracy_test'].mean()
-            acc_test_std = group_byz.groupby('epoch')['accuracy_test'].std()
-
-            acc_train_mean = group_byz.groupby('epoch')['accuracy_train'].mean()
-            acc_train_std = group_byz.groupby('epoch')['accuracy_train'].std()
-
-            plt.plot(acc_train_mean.index, acc_train_mean.values, label=f'TRAIN - byz: {nb_byz}, agg: {aggregator}',
-                     linestyle='dashed')
-            plt.fill_between(acc_train_mean.index, acc_train_mean.values - acc_train_std.values,
-                             acc_train_mean.values + acc_train_std.values, alpha=0.2)
-            plt.plot(acc_test_mean.index, acc_test_mean.values, label=f'TEST - byz: {nb_byz}, agg: {aggregator}',
-                     linestyle='solid', color=plt.gca().lines[-1].get_color())
-            plt.fill_between(acc_test_mean.index, acc_test_mean.values - acc_test_std.values,
-                             acc_test_mean.values + acc_test_std.values, alpha=0.2,
-                             color=plt.gca().lines[-1].get_color())
-
-        # add None aggregator
+        # add None aggregator for accuracy
         group_none = df[
             (df['nb_nodes'] == nb_nodes) & (df['batch_size'] == batch_size) & (df['nb_epochs'] == nb_epochs) & (
                     df['aggregator'] == 'None') & (df['lr'] == lr)]
@@ -84,24 +65,117 @@ def plot():
         acc_train_mean_none = group_none.groupby('epoch')['accuracy_train'].mean()
         acc_train_std_none = group_none.groupby('epoch')['accuracy_train'].std()
 
-        plt.plot(acc_train_mean_none.index, acc_train_mean_none.values, label='TRAIN - agg: None',
-                 linestyle='dashed')
-        plt.fill_between(acc_train_mean_none.index, acc_train_mean_none.values - acc_train_std_none.values,
-                         acc_train_mean_none.values + acc_train_std_none.values, alpha=0.2)
-        plt.plot(acc_test_mean_none.index, acc_test_mean_none.values, label='TEST - agg: None',
-                 linestyle='solid', color=plt.gca().lines[-1].get_color())
-        plt.fill_between(acc_test_mean_none.index, acc_test_mean_none.values - acc_test_std_none.values,
-                         acc_test_mean_none.values + acc_test_std_none.values, alpha=0.2,
-                         color=plt.gca().lines[-1].get_color())
+        ax[0].plot(acc_train_mean_none.index, acc_train_mean_none.values, label='TRAIN - agg: None',
+                   linestyle='dashed')
+        ax[0].fill_between(acc_train_mean_none.index, acc_train_mean_none.values - acc_train_std_none.values,
+                           acc_train_mean_none.values + acc_train_std_none.values, alpha=0.2)
+        ax[0].plot(acc_test_mean_none.index, acc_test_mean_none.values, label='TEST - agg: None',
+                   linestyle='solid', color=ax[0].get_lines()[-1].get_color())
+        ax[0].fill_between(acc_test_mean_none.index, acc_test_mean_none.values - acc_test_std_none.values,
+                           acc_test_mean_none.values + acc_test_std_none.values, alpha=0.2,
+                           color=ax[0].get_lines()[-1].get_color())
 
-        plt.ylim(0.6, 1)
-        plt.legend()
+        # generalization_gap = accuracy_test - accuracy_train
+        generalization_gap_mean = group_none.groupby('epoch') \
+            .apply(lambda x: (x['accuracy_test'] - x['accuracy_train']).mean())
+        generalization_gap_std = group_none.groupby('epoch') \
+            .apply(lambda x: (x['accuracy_test'] - x['accuracy_train']).std())
 
-        plt.xlabel('Epoch')
-        plt.ylabel('Accuracy')
+        ax[1].plot(generalization_gap_mean.index, generalization_gap_mean.values,
+                   label='GAP - agg: None',
+                   linestyle='solid')
 
-        plt.title(f'Accuracy evolution of {aggregator} with {nb_nodes} nodes, '
-                  f'{nb_epochs} epochs and batch size {batch_size}')
+        ax[1].fill_between(generalization_gap_mean.index,
+                           generalization_gap_mean.values - generalization_gap_std.values,
+                           generalization_gap_mean.values + generalization_gap_std.values, alpha=0.2)
+
+        # add None aggregator for loss
+        loss_test_mean_none = group_none.groupby('epoch')['loss_test'].mean()
+        loss_test_std_none = group_none.groupby('epoch')['loss_test'].std()
+
+        loss_train_mean_none = group_none.groupby('epoch')['loss_train'].mean()
+        loss_train_std_none = group_none.groupby('epoch')['loss_train'].std()
+
+        ax[2].plot(loss_train_mean_none.index, loss_train_mean_none.values, label='TRAIN - agg: None',
+                   linestyle='dashed')
+        ax[2].fill_between(loss_train_mean_none.index, loss_train_mean_none.values - loss_train_std_none.values,
+                           loss_train_mean_none.values + loss_train_std_none.values, alpha=0.2)
+        ax[2].plot(loss_test_mean_none.index, loss_test_mean_none.values, label='TEST - agg: None',
+                   linestyle='solid', color=ax[0].get_lines()[-1].get_color())
+        ax[2].fill_between(loss_test_mean_none.index, loss_test_mean_none.values - loss_test_std_none.values,
+                           loss_test_mean_none.values + loss_test_std_none.values, alpha=0.2,
+                           color=ax[0].get_lines()[-1].get_color())
+
+        for nb_byz in group['nb_byz'].unique():
+            group_byz = group[group['nb_byz'] == nb_byz]
+
+            # accuracy
+
+            acc_test_mean = group_byz.groupby('epoch')['accuracy_test'].mean()
+            acc_test_std = group_byz.groupby('epoch')['accuracy_test'].std()
+
+            acc_train_mean = group_byz.groupby('epoch')['accuracy_train'].mean()
+            acc_train_std = group_byz.groupby('epoch')['accuracy_train'].std()
+
+            ax[0].plot(acc_train_mean.index, acc_train_mean.values, label=f'TRAIN - byz: {nb_byz}, agg: {aggregator}',
+                       linestyle='dashed')
+            ax[0].fill_between(acc_train_mean.index, acc_train_mean.values - acc_train_std.values,
+                               acc_train_mean.values + acc_train_std.values, alpha=0.2)
+            ax[0].plot(acc_test_mean.index, acc_test_mean.values, label=f'TEST - byz: {nb_byz}, agg: {aggregator}',
+                       linestyle='solid', color=ax[0].get_lines()[-1].get_color())
+            ax[0].fill_between(acc_test_mean.index, acc_test_mean.values - acc_test_std.values,
+                               acc_test_mean.values + acc_test_std.values, alpha=0.2,
+                               color=ax[0].get_lines()[-1].get_color())
+
+            # accuracy_test - accuracy_train
+            generalization_gap_mean = group_byz.groupby('epoch') \
+                .apply(lambda x: (x['accuracy_test'] - x['accuracy_train']).mean())
+            generalization_gap_std = group_byz.groupby('epoch') \
+                .apply(lambda x: (x['accuracy_test'] - x['accuracy_train']).std())
+
+            ax[1].plot(generalization_gap_mean.index, generalization_gap_mean.values,
+                       label=f'GAP - byz: {nb_byz}, agg: {aggregator}',
+                       linestyle='solid')
+
+            ax[1].fill_between(generalization_gap_mean.index,
+                               generalization_gap_mean.values - generalization_gap_std.values,
+                               generalization_gap_mean.values + generalization_gap_std.values, alpha=0.2)
+
+            # loss
+            loss_test_mean = group_byz.groupby('epoch')['loss_test'].mean()
+            loss_test_std = group_byz.groupby('epoch')['loss_test'].std()
+
+            loss_train_mean = group_byz.groupby('epoch')['loss_train'].mean()
+            loss_train_std = group_byz.groupby('epoch')['loss_train'].std()
+
+            ax[2].plot(loss_train_mean.index, loss_train_mean.values, label=f'TRAIN - byz: {nb_byz}, agg: {aggregator}',
+                       linestyle='dashed')
+            ax[2].fill_between(loss_train_mean.index, loss_train_mean.values - loss_train_std.values,
+                               loss_train_mean.values + loss_train_std.values, alpha=0.2)
+            ax[2].plot(loss_test_mean.index, loss_test_mean.values, label=f'TEST - byz: {nb_byz}, agg: {aggregator}',
+                       linestyle='solid', color=ax[0].get_lines()[-1].get_color())
+            ax[2].fill_between(loss_test_mean.index, loss_test_mean.values - loss_test_std.values,
+                               loss_test_mean.values + loss_test_std.values, alpha=0.2,
+                               color=ax[0].get_lines()[-1].get_color())
+
+        ax[0].set_ylim([0.6, 1.0])
+        ax[0].legend()
+        ax[1].legend()
+        ax[2].legend()
+
+        ax[0].set_xlabel('Epoch')
+        ax[0].set_ylabel('Accuracy')
+        ax[1].set_xlabel('Epoch')
+        ax[1].set_ylabel('Generalization gap')
+        ax[2].set_xlabel('Epoch')
+        ax[2].set_ylabel('Loss')
+
+        ax[0].set_title(f'Accuracy evolution of {aggregator} with {nb_nodes} nodes, '
+                        f'{nb_epochs} epochs and batch size {batch_size}')
+        ax[1].set_title(f'Generalization gap evolution of {aggregator} with {nb_nodes} nodes, '
+                        f'{nb_epochs} epochs and batch size {batch_size}')
+        ax[2].set_title(f'Loss evolution of {aggregator} with {nb_nodes} nodes, '
+                        f'{nb_epochs} epochs and batch size {batch_size}')
 
         filename = f'z-nodes_{nb_nodes}_epochs_{nb_epochs}_batch_{batch_size}_agg_' \
                    f'{aggregator}.png'
