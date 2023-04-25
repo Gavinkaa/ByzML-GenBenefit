53ecc87c41d284f5fbf058854c1fa5797f82d2e3
Gavinkaa <ludovic.burnier@epfl.ch>
2023-04-25 11:49:50 +0200

    add pandas as dependencies and rework of visu.py

--- src/visu.py
+++ src/visu.py
@@ -1,118 +1,120 @@
-import matplotlib.pyplot as plt
-import csv
 import os
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import pandas as pd
+
 # ------ SETTINGS ------
-INPUT_FOLDER = '../results/MNIST/batch_1100/'
-OUTPUT_FOLDER = '../results/MNIST/batch_1100/'
+INPUT_FOLDER = '../results/CIFAR-10'
+OUTPUT_FOLDER = '../results/CIFAR-10'
 
 
-def plot(nb_of_nodes: list[str], batch_size: list[str], nb_epochs: list[str], aggregators: list[str], files: list[str]):
+def plot():
     plt.rcParams['figure.figsize'] = (10, 8)
 
+    files = os.listdir(Path(INPUT_FOLDER))
+
+    df = pd.DataFrame()
+
     for file in files:
-        if file.endswith('.csv'):
-            file_labels = file.split('_')
-            file_nb_nodes = file_labels[1]
-            file_nb_byz = file_labels[3]
-            file_batch_size = file_labels[5]
-            file_nb_epochs = file_labels[7]
-            aggregator = file_labels[9].split('Aggregator')[0]
-            # file_lr = file_labels[11]
-            # file_seed = file_labels[13].split('.csv')[0]
+        if not file.endswith('.csv'):
+            continue
+        file_labels = file.split('_')
+        file_nb_nodes = file_labels[1]
+        file_nb_byz = file_labels[3]
+        file_batch_size = file_labels[5]
+        file_nb_epochs = file_labels[7]
+        aggregator = file_labels[9].split('Aggregator')[0]
+        file_lr = file_labels[11]
+        file_seed = file_labels[13].split('.csv')[0]
 
-            if file_nb_nodes not in nb_of_nodes or \
-                    file_batch_size not in batch_size or \
-                    file_nb_epochs not in nb_epochs or \
-                    aggregator not in aggregators:
-                continue
+        local_df = pd.read_csv(Path(INPUT_FOLDER, file))
+        local_df['nb_nodes'] = file_nb_nodes
+        local_df['nb_byz'] = file_nb_byz
+        local_df['batch_size'] = file_batch_size
+        local_df['nb_epochs'] = file_nb_epochs
+        local_df['aggregator'] = aggregator
+        local_df['lr'] = file_lr
+        local_df['seed'] = file_seed
 
-            accuracies_train = []
-            accuracies_test = []
-            with open(Path(INPUT_FOLDER, file), 'r') as csvfile:
-                plots = csv.reader(csvfile, delimiter=',')
-                # skip header
-                next(plots)
-                for row in plots:
-                    accuracies_train.append(float(row[1]))
-                    accuracies_test.append(float(row[2]))
+        df = pd.concat([df, local_df])
 
-            # plot the moving average
-            accuracies_train_ma = []
-            accuracies_test_ma = []
-            ma_window = 10
-            for i in range(len(accuracies_train)):
-                if i < ma_window:
-                    accuracies_train_ma.append(sum(accuracies_train[:i]) / (i + 1))
-                    accuracies_test_ma.append(sum(accuracies_test[:i]) / (i + 1))
-                else:
-                    accuracies_train_ma.append(sum(accuracies_train[i - ma_window:i]) / ma_window)
-                    accuracies_test_ma.append(sum(accuracies_test[i - ma_window:i]) / ma_window)
+    # Group the data by the tuple
+    grouped = df.groupby(['nb_nodes', 'batch_size', 'nb_epochs', 'aggregator', 'lr'])
 
-            # plot train in dashed line
-            # for test use the same color as the train
+    # Loop over each group and plot the mean accuracy over epochs
+    for name, group in grouped:
+        nb_nodes = name[0]
+        batch_size = name[1]
+        nb_epochs = name[2]
+        aggregator = name[3]
+        lr = name[4]
 
-            plt.plot(accuracies_train_ma, label=f'TRAIN nodes: {file_nb_nodes}, byz: {file_nb_byz}, agg: {aggregator}',
+        if aggregator == 'None':
+            continue
+
+        plt.figure()
+
+        for nb_byz in group['nb_byz'].unique():
+            group_byz = group[group['nb_byz'] == nb_byz]
+
+            acc_test_mean = group_byz.groupby('epoch')['accuracy_test'].mean()
+            acc_test_std = group_byz.groupby('epoch')['accuracy_test'].std()
+
+            acc_train_mean = group_byz.groupby('epoch')['accuracy_train'].mean()
+            acc_train_std = group_byz.groupby('epoch')['accuracy_train'].std()
+
+            plt.plot(acc_train_mean.index, acc_train_mean.values, label=f'TRAIN - byz: {nb_byz}, agg: {aggregator}',
                      linestyle='dashed')
+            plt.fill_between(acc_train_mean.index, acc_train_mean.values - acc_train_std.values,
+                             acc_train_mean.values + acc_train_std.values, alpha=0.2)
+            plt.plot(acc_test_mean.index, acc_test_mean.values, label=f'TEST - byz: {nb_byz}, agg: {aggregator}',
+                     linestyle='solid', color=plt.gca().lines[-1].get_color())
+            plt.fill_between(acc_test_mean.index, acc_test_mean.values - acc_test_std.values,
+                             acc_test_mean.values + acc_test_std.values, alpha=0.2,
+                             color=plt.gca().lines[-1].get_color())
 
-            plt.plot(accuracies_test_ma, label=f'TEST nodes: {file_nb_nodes}, byz: {file_nb_byz}, agg: {aggregator}',
-                     linestyle='solid',
-                     color=plt.gca().lines[-1].get_color())
+        # add None aggregator
+        group_none = df[
+            (df['nb_nodes'] == nb_nodes) & (df['batch_size'] == batch_size) & (df['nb_epochs'] == nb_epochs) & (
+                    df['aggregator'] == 'None') & (df['lr'] == lr)]
+        acc_test_mean_none = group_none.groupby('epoch')['accuracy_test'].mean()
+        acc_test_std_none = group_none.groupby('epoch')['accuracy_test'].std()
 
-    # check if there is at least one plot
-    if len(plt.gca().lines) == 0:
-        return
+        acc_train_mean_none = group_none.groupby('epoch')['accuracy_train'].mean()
+        acc_train_std_none = group_none.groupby('epoch')['accuracy_train'].std()
 
-    plt.ylim(0.95, 1)
-    plt.legend()
-    plt.title(f'Accuracy evolution of {[x for x in aggregators if x != "None"][0]} with {nb_of_nodes} nodes, '
-              f'{nb_epochs} epochs and batch size {batch_size}')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
+        plt.plot(acc_train_mean_none.index, acc_train_mean_none.values, label='TRAIN - agg: None',
+                 linestyle='dashed')
+        plt.fill_between(acc_train_mean_none.index, acc_train_mean_none.values - acc_train_std_none.values,
+                         acc_train_mean_none.values + acc_train_std_none.values, alpha=0.2)
+        plt.plot(acc_test_mean_none.index, acc_test_mean_none.values, label='TEST - agg: None',
+                 linestyle='solid', color=plt.gca().lines[-1].get_color())
+        plt.fill_between(acc_test_mean_none.index, acc_test_mean_none.values - acc_test_std_none.values,
+                         acc_test_mean_none.values + acc_test_std_none.values, alpha=0.2,
+                         color=plt.gca().lines[-1].get_color())
 
-    filename = f'nodes_{nb_of_nodes}_epochs_{nb_epochs}_batch_{batch_size}_agg_' \
-               f'{[a for a in aggregators if a != "None"][0]}.png'
+        plt.ylim(0.6, 1)
+        plt.legend()
 
-    plt.savefig(Path(OUTPUT_FOLDER, filename))
-    # plt.show()
-    plt.clf()
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+
+        plt.title(f'Accuracy evolution of {aggregator} with {nb_nodes} nodes, '
+                  f'{nb_epochs} epochs and batch size {batch_size}')
+
+        filename = f'z-nodes_{nb_nodes}_epochs_{nb_epochs}_batch_{batch_size}_agg_' \
+                   f'{aggregator}.png'
+
+        # plt.show()
+        print((Path(OUTPUT_FOLDER, filename)))
+        plt.savefig(Path(OUTPUT_FOLDER, filename))
+        plt.close()
+        # plt.clf()
 
 
 def main():
-    # first we list all different values of the parameters
-    files = os.listdir(Path(INPUT_FOLDER))
-    # make sure we have a predictable order
-    files.sort()
-    # if filename contains 'None' put it at the beginning
-    files = [file for file in files if 'None' in file] + [file for file in files if
-                                                          'None' not in file]
-
-    nb_nodes = set()
-    batch_sizes = set()
-    epochs = set()
-    agg = set()
-    # lr = set()
-    # seed = set()
-
-    for file in files:
-        if file.endswith('.csv'):
-            labels = file.split('_')
-            nb_nodes.add(labels[1])
-            batch_sizes.add(labels[5])
-            epochs.add(labels[7])
-            agg.add(labels[9].split('Aggregator')[0])
-            # lr.add(labels[11])
-            # seed.add(labels[13].split('.csv')[0])
-
-    agg.remove('None')
-
-    for nb_of_nodes in nb_nodes:
-        for batch_size in batch_sizes:
-            for nb_epochs in epochs:
-                for aggregator in agg:
-                    if aggregator != 'None':
-                        plot([nb_of_nodes], [batch_size], [nb_epochs], ['None', aggregator], files)
+    plot()
 
 
 if __name__ == '__main__':
